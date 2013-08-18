72551ffd71c252f5bd6f22e6a315668897c907e3
stef <stefan.marsiske@gmail.com>
2013-08-18 21:14:47 +0200

    [mod] renamed pbp in test.sh

--- test.sh
+++ test.sh
@@ -2,9 +2,9 @@
 
 rm -rf test.pbp
 # create some keys
-./pep.py -g -n alice -b test.pbp
-./pep.py -g -n bob -b test.pbp
-./pep.py -g -n carol -b test.pbp
+./pbp.py -g -n alice -b test.pbp
+./pbp.py -g -n bob -b test.pbp
+./pbp.py -g -n carol -b test.pbp
 
 # test msg
 cat >test.pbp/howdy.txt <<EOF
@@ -12,16 +12,16 @@ hello world
 EOF
 
 # public key crypto test
-./pep.py -c -S alice -r bob -i test.pbp/howdy.txt -b test.pbp
+./pbp.py -c -S alice -r bob -i test.pbp/howdy.txt -b test.pbp
 # decrypt
-./pep.py -d -S bob -i test.pbp/howdy.txt.pbp -b test.pbp
+./pbp.py -d -S bob -i test.pbp/howdy.txt.pbp -b test.pbp
 
 # secret key crypto test
-./pep.py -c -i test.pbp/howdy.txt
+./pbp.py -c -i test.pbp/howdy.txt
 # decrypt
-./pep.py -d -i test.pbp/howdy.txt.pbp
+./pbp.py -d -i test.pbp/howdy.txt.pbp
 
 # public key signature test
-./pep.py -s -S alice -i test.pbp/howdy.txt -b test.pbp
+./pbp.py -s -S alice -i test.pbp/howdy.txt -b test.pbp
 # verify
-./pep.py -v -i test.pbp/howdy.txt.sig -b test.pbp
+./pbp.py -v -i test.pbp/howdy.txt.sig -b test.pbp
