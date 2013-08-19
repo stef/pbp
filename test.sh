280d2058e779817b77395b0b33fcc1385651f9df
stef <stefan.marsiske@gmail.com>
2013-08-19 03:21:36 +0200

    [enh] added tests for experimental pfs mode

--- test.sh
+++ test.sh
@@ -34,3 +34,20 @@ EOF
 # check sigs on carols key
 ./pbp.py -C -n carol -b test-pbp
 ./pbp.py -C -n bob -b test-pbp
+
+# test PFS mode
+rm test-pbp/sk/.alice/bob test-pbp/sk/.bob/alice
+./pbp.py -e -S alice -r bob -b test-pbp -i test-pbp/howdy.txt -o /tmp/a
+./pbp.py -E -S bob -r alice -b test-pbp/ -i /tmp/a -o /tmp/b
+./pbp.py -e -S bob -r alice -b test-pbp/ -i /tmp/b -o /tmp/c
+./pbp.py -E -S alice -r bob -b test-pbp/ -i /tmp/c -o /tmp/d
+./pbp.py -e -S alice -r bob -b test-pbp/ -i /tmp/d -o /tmp/e
+./pbp.py -E -S bob -r alice -b test-pbp/ -i /tmp/e -o /tmp/f
+./pbp.py -e -S bob -r alice -b test-pbp/ -i /tmp/f -o /tmp/g
+./pbp.py -E -S alice -r bob -b test-pbp/ -i /tmp/g -o /tmp/h
+./pbp.py -e -S alice -r bob -b test-pbp/ -i /tmp/h -o /tmp/i
+./pbp.py -E -S bob -r alice -b test-pbp/ -i /tmp/i -o /tmp/j
+
+echo "difference between howdy.txt and /tmp/j"
+diff /tmp/j test-pbp/howdy.txt && echo None
+rm test-pbp/sk/.alice/bob test-pbp/sk/.bob/alice /tmp/[abcdefghij]
