3e114ad752fcc0888781c062a42f63a442edb636
András Veres-Szentkirályi <vsza@vsza.hu>
2013-08-21 23:01:45 +0200

    [enh] added verification test without key in store

--- test_pbp.py
+++ test_pbp.py
@@ -102,6 +102,13 @@ class TestPBP(unittest.TestCase):
         malformed = ''.join(chr(ord(c) ^ 42) for c in signed)
         self.assertTrue(pbp.verify(malformed, basedir=self.pbp_path) is None)
 
+    def test_sign_no_key(self):
+        self_key = self.gen_key()
+        signed = pbp.sign(MESSAGE, self=self_key)
+        rmtree(self.pbp_path)
+        self.gen_key()
+        self.assertTrue(pbp.verify(signed, basedir=self.pbp_path) is None)
+
     def test_sign(self):
         self_key = self.gen_key()
         self.assertTrue(pbp.verify(pbp.sign(MESSAGE, self=self_key),
