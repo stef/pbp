4249f2e3192340b3c7203f931002eff3f2d31d5e
stef <stefan.marsiske@gmail.com>
2013-08-22 02:04:55 +0200

    [mod] (c)

--- chaining.py
+++ chaining.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#    This program is free software: you can redistribute it and/or modify it
+#    under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of
+#    the License, or (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Affero General Public License for more details.
+
+#    You should have received a copy of the GNU Affero General Public
+#    License along with this program. If not, see
+#    <http://www.gnu.org/licenses/>.
+
+# (C) 2013 by Stefan Marsiske, <s@ctrlc.hu>
+
 import nacl
 from pbp import Identity, die, defaultbase as basedir
 from utils import b85encode
