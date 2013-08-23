ec771a957a283ddcf7ea8c8bea215322bcdf6f81
stef <stefan.marsiske@gmail.com>
2013-08-23 22:45:53 +0200

    [enh] ported to libsodium

---
+++ nacl.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+
+import sodium
+
+crypto_box_NONCEBYTES = sodium.lib.crypto_box_NONCEBYTES
+crypto_box_PUBLICKEYBYTES = sodium.lib.crypto_box_PUBLICKEYBYTES
+crypto_box_SECRETKEYBYTES = sodium.lib.crypto_box_SECRETKEYBYTES
+crypto_box_ZEROBYTES = sodium.lib.crypto_box_ZEROBYTES
+crypto_box_BOXZEROBYTES = sodium.lib.crypto_box_BOXZEROBYTES
+crypto_secretbox_KEYBYTES = sodium.lib.crypto_secretbox_KEYBYTES
+crypto_secretbox_NONCEBYTES = sodium.lib.crypto_secretbox_NONCEBYTES
+crypto_secretbox_KEYBYTES = sodium.lib.crypto_secretbox_KEYBYTES
+crypto_secretbox_ZEROBYTES = sodium.lib.crypto_box_ZEROBYTES
+crypto_secretbox_BOXZEROBYTES = sodium.lib.crypto_box_BOXZEROBYTES
+crypto_sign_PUBLICKEYBYTES = sodium.lib.crypto_sign_PUBLICKEYBYTES
+crypto_sign_SECRETKEYBYTES = sodium.lib.crypto_sign_SECRETKEYBYTES
+crypto_stream_KEYBYTES = sodium.lib.crypto_stream_KEYBYTES
+crypto_stream_NONCEBYTES = sodium.lib.crypto_stream_NONCEBYTES
+crypto_hash_BYTES = sodium.lib.crypto_hash_BYTES
+crypto_scalarmult_curve25519_BYTES = sodium.lib.crypto_scalarmult_curve25519_BYTES
+crypto_scalarmult_BYTES = sodium.lib.crypto_scalarmult_curve25519_BYTES
+crypto_sign_BYTES = sodium.lib.crypto_sign_BYTES
+
+def crypto_scalarmult_curve25519(n,p):
+    buf = sodium.ffi.new("unsigned char[]", crypto_hash_BYTES)
+    sodium.lib.crypto_scalarmult_curve25519(buf, n, p)
+    return sodium.ffi.buffer(buf, crypto_scalarmult_BYTES)[:]
+
+def crypto_scalarmult_curve25519_base(n):
+    buf = sodium.ffi.new("unsigned char[]", crypto_hash_BYTES)
+    sodium.lib.crypto_scalarmult_curve25519_base(buf, n)
+    return sodium.ffi.buffer(buf, crypto_scalarmult_BYTES)[:]
+
+def crypto_hash_sha256(m):
+    buf = sodium.ffi.new("unsigned char[]", crypto_hash_BYTES)
+    sodium.lib.crypto_hash_sha256(buf, m, len(m))
+    return sodium.ffi.buffer(buf, crypto_hash_BYTES)[:]
+
+def crypto_hash_sha512(m):
+    buf = sodium.ffi.new("unsigned char[]", crypto_hash_BYTES)
+    sodium.lib.crypto_hash_sha512(buf, m, len(m))
+    return sodium.ffi.buffer(buf, crypto_hash_BYTES)[:]
+
+def randombytes(l):
+    buf = sodium.ffi.new("unsigned char[]", l)
+    sodium.lib.randombytes(buf, l)
+    return sodium.ffi.buffer(buf, l)[:]
+
+def crypto_box_keypair():
+    pk = sodium.ffi.new("unsigned char[]", crypto_box_PUBLICKEYBYTES)
+    sk = sodium.ffi.new("unsigned char[]", crypto_box_SECRETKEYBYTES)
+    if not sodium.lib.crypto_box_keypair(pk, sk):
+        raise ValueError
+    pk = sodium.ffi.buffer(pk, crypto_box_PUBLICKEYBYTES)[:]
+    sk = sodium.ffi.buffer(sk, crypto_box_SECRETKEYBYTES)[:]
+    return (pk, sk)
+
+def crypto_box(msg, nonce, pk, sk):
+    padded = b"\x00" * crypto_box_ZEROBYTES + msg
+    c = sodium.ffi.new("unsigned char[]", len(padded))
+    if not sodium.lib.crypto_box(c, padded, len(padded), nonce, pk, sk):
+        raise ValueError
+    return sodium.ffi.buffer(c, len(padded))[crypto_box_BOXZEROBYTES:]
+
+def crypto_box_open(c, nonce, pk, sk):
+    padded = b"\x00" * crypto_box_BOXZEROBYTES + c
+    msg = sodium.ffi.new("unsigned char[]", len(padded))
+    if not sodium.lib.crypto_box_open(msg, padded, len(padded), nonce, pk, sk):
+        raise ValueError
+    return sodium.ffi.buffer(msg, len(padded))[crypto_box_ZEROBYTES:]
+
+def crypto_secretbox(msg, nonce, k):
+    padded = b"\x00" * crypto_secretbox_ZEROBYTES + msg
+    c = sodium.ffi.new("unsigned char[]", len(padded))
+    if not sodium.lib.crypto_secretbox(c, padded, len(padded), nonce, k):
+        raise ValueError
+    return sodium.ffi.buffer(c, len(padded))[crypto_secretbox_BOXZEROBYTES:]
+
+def crypto_secretbox_open(c, nonce, k):
+    padded = b"\x00" * crypto_secretbox_BOXZEROBYTES + c
+    msg = sodium.ffi.new("unsigned char[]", len(padded))
+    if not sodium.lib.crypto_secretbox_open(msg, padded, len(padded), nonce, k):
+        raise ValueError
+    return sodium.ffi.buffer(msg, len(padded))[crypto_secretbox_ZEROBYTES:]
+
+def crypto_sign_keypair():
+    pk = sodium.ffi.new("unsigned char[]", crypto_sign_PUBLICKEYBYTES)
+    sk = sodium.ffi.new("unsigned char[]", crypto_sign_SECRETKEYBYTES)
+    if not sodium.lib.crypto_sign_keypair(pk, sk):
+        raise ValueError
+    pk = sodium.ffi.buffer(pk, crypto_sign_PUBLICKEYBYTES)[:]
+    sk = sodium.ffi.buffer(sk, crypto_sign_SECRETKEYBYTES)[:]
+    return (pk, sk)
+
+def crypto_sign(m, sk):
+    smsg = sodium.ffi.new("unsigned char[]", len(m)+crypto_sign_BYTES)
+    smsglen = sodium.ffi.new("unsigned long long *")
+    if not sodium.lib.crypto_sign(smsg, smsglen, m, len(m), sk):
+        raise ValueError
+    return sodium.ffi.buffer(smsg, smsglen[0])[:]
+
+def crypto_sign_open(sm, pk):
+    msg = sodium.ffi.new("unsigned char[]", len(sm))
+    msglen = sodium.ffi.new("unsigned long long *")
+    if not sodium.lib.crypto_sign_open(msg, msglen, sm, len(sm), pk):
+        raise ValueError
+    return sodium.ffi.buffer(msg, msglen[0])[:]
+
+def test():
+    pk, sk = crypto_box_keypair()
+    n = randombytes(crypto_box_NONCEBYTES)
+    c = crypto_box("howdy", n, pk, sk)
+    print crypto_box_open(c, n, pk, sk)
+
+    k = randombytes(crypto_secretbox_KEYBYTES)
+    n = randombytes(crypto_secretbox_NONCEBYTES)
+    c = crypto_secretbox("howdy", n, k)
+    print crypto_secretbox_open(c, n, k)
+
+    print repr(crypto_hash_sha512('howdy'))
+    print repr(crypto_hash_sha256('howdy'))
+
+    s = crypto_scalarmult_curve25519_base(randombytes(crypto_scalarmult_BYTES))
+    r = crypto_scalarmult_curve25519_base(randombytes(crypto_scalarmult_BYTES))
+    print 'scalarmult'
+    print repr(crypto_scalarmult_curve25519(s,r))
+
+    pk, sk = crypto_sign_keypair()
+    signed = crypto_sign('howdy',sk)
+    print crypto_sign_open(signed, pk)
+    print crypto_sign_BYTES
+    print repr(crypto_sign('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',sk))
+
+if __name__ == '__main__':
+    test()
---
+++ sodium.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+"""
+CFFI interface to NaCl and libsodium library
+"""
+from __future__ import absolute_import
+from __future__ import division
+
+import functools
+
+from cffi import FFI
+
+
+__all__ = ["ffi", "lib"]
+
+
+ffi = FFI()
+ffi.cdef(
+    # Secret Key Encryption
+    """
+        static const int crypto_secretbox_KEYBYTES;
+        static const int crypto_secretbox_NONCEBYTES;
+        static const int crypto_secretbox_ZEROBYTES;
+        static const int crypto_secretbox_BOXZEROBYTES;
+        static const int crypto_secretbox_KEYBYTES;
+
+        int crypto_secretbox(unsigned char *c, const unsigned char *m, unsigned long long mlen, const unsigned char *n, const unsigned char *k);
+        int crypto_secretbox_open(unsigned char *m, const unsigned char *c, unsigned long long clen, const unsigned char *n, const unsigned char *k);
+    """
+
+    # Public Key Encryption - Signatures
+    """
+        static const int crypto_sign_PUBLICKEYBYTES;
+        static const int crypto_sign_SECRETKEYBYTES;
+        static const int crypto_sign_BYTES;
+
+        int crypto_sign_seed_keypair(unsigned char *pk, unsigned char *sk, unsigned char *seed);
+        int crypto_sign_keypair(unsigned char *pk, unsigned char *sk);
+        int crypto_sign(unsigned char *sm, unsigned long long *smlen, const unsigned char *m, unsigned long long mlen, const unsigned char *sk);
+        int crypto_sign_open(unsigned char *m, unsigned long long *mlen, const unsigned char *sm, unsigned long long smlen, const unsigned char *pk);
+    """
+
+    # Public Key Encryption
+    """
+        static const int crypto_box_PUBLICKEYBYTES;
+        static const int crypto_box_SECRETKEYBYTES;
+        static const int crypto_box_BEFORENMBYTES;
+        static const int crypto_box_NONCEBYTES;
+        static const int crypto_box_ZEROBYTES;
+        static const int crypto_box_BOXZEROBYTES;
+
+        int crypto_box_keypair(unsigned char *pk, unsigned char *sk);
+        int crypto_box(unsigned char *c, const unsigned char *m, unsigned long long mlen, const unsigned char *n, const unsigned char *pk, const unsigned char *sk);
+        int crypto_box_open(unsigned char *m, const unsigned char *c, unsigned long long clen, const unsigned char *n, const unsigned char *pk, const unsigned char *sk);
+        int crypto_box_afternm(unsigned char *c, const unsigned char *m, unsigned long long mlen, const unsigned char *n, const unsigned char *k);
+        int crypto_box_open_afternm(unsigned char *m, const unsigned char *c, unsigned long long clen, const unsigned char *n, const unsigned char *k);
+        int crypto_box_beforenm(unsigned char *k, const unsigned char *pk, const unsigned char *sk);
+    """
+
+    # Hashing
+    """
+        static const int crypto_hash_BYTES;
+        static const int crypto_hash_sha256_BYTES;
+        static const int crypto_hash_sha512_BYTES;
+
+        int crypto_hash(unsigned char *out, const unsigned char *in, unsigned long long inlen);
+        int crypto_hash_sha256(unsigned char *out, const unsigned char *in, unsigned long long inlen);
+        int crypto_hash_sha512(unsigned char *out, const unsigned char *in, unsigned long long inlen);
+    """
+
+    # Secure Random
+    """
+        void randombytes(unsigned char * const buf, const unsigned long long buf_len);
+    """
+
+    # Low Level - Scalar Multiplication
+    """
+        static const int crypto_scalarmult_curve25519_BYTES;
+        int crypto_scalarmult_curve25519_base(unsigned char *q, const unsigned char *n);
+        int crypto_scalarmult_curve25519(unsigned char *q, const unsigned char *n, unsigned char *q);
+    """
+
+    # Stream  Encryption
+    """
+        static const int crypto_stream_KEYBYTES;
+        static const int crypto_stream_NONCEBYTES;
+
+        int crypto_stream(unsigned char *c, unsigned long long clen, const unsigned char *n, const unsigned char *k);
+        int crypto_stream_xor(unsigned char *m, unsigned char *c, unsigned long long clen, const unsigned char *n, const unsigned char *k);
+    """
+)
+
+
+lib = ffi.verify("#include <sodium.h>", libraries=["sodium"])
+
+
+# This works around a bug in PyPy where CFFI exposed functions do not have a
+#   __name__ attribute. See https://bugs.pypy.org/issue1452
+def wraps(wrapped):
+    def inner(func):
+        if hasattr(wrapped, "__name__"):
+            return functools.wraps(wrapped)(func)
+        else:
+            return func
+    return inner
+
+
+# A lot of the functions in nacl return 0 for success and a negative integer
+#   for failure. This is inconvenient in Python as 0 is a falsey value while
+#   negative integers are truthy. This wrapper has them return True/False as
+#   you'd expect in Python
+def wrap_nacl_function(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        ret = func(*args, **kwargs)
+        return ret == 0
+    return wrapper
+
+lib.crypto_secretbox = wrap_nacl_function(lib.crypto_secretbox)
+lib.crypto_secretbox_open = wrap_nacl_function(lib.crypto_secretbox_open)
+
+lib.crypto_sign_seed_keypair = wrap_nacl_function(lib.crypto_sign_seed_keypair)
+lib.crypto_sign_keypair = wrap_nacl_function(lib.crypto_sign_keypair)
+lib.crypto_sign = wrap_nacl_function(lib.crypto_sign)
+lib.crypto_sign_open = wrap_nacl_function(lib.crypto_sign_open)
+
+lib.crypto_box_keypair = wrap_nacl_function(lib.crypto_box_keypair)
+lib.crypto_box = wrap_nacl_function(lib.crypto_box)
+lib.crypto_box_open = wrap_nacl_function(lib.crypto_box_open)
+lib.crypto_box_afternm = wrap_nacl_function(lib.crypto_box_afternm)
+lib.crypto_box_open_afternm = wrap_nacl_function(lib.crypto_box_open_afternm)
+lib.crypto_box_beforenm = wrap_nacl_function(lib.crypto_box_beforenm)
+
+lib.crypto_hash = wrap_nacl_function(lib.crypto_hash)
+lib.crypto_hash_sha256 = wrap_nacl_function(lib.crypto_hash_sha256)
+lib.crypto_hash_sha512 = wrap_nacl_function(lib.crypto_hash_sha512)
+
+lib.crypto_scalarmult_curve25519_base = wrap_nacl_function(lib.crypto_scalarmult_curve25519_base)
+lib.crypto_scalarmult_curve25519 = wrap_nacl_function(lib.crypto_scalarmult_curve25519)
+
+lib.randombytes = wrap_nacl_function(lib.randombytes)
+
+lib.crypto_stream_xor = wrap_nacl_function(lib.crypto_stream_xor)
+lib.crypto_stream = wrap_nacl_function(lib.crypto_stream)
