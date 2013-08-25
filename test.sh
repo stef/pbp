866826842495bb3ac933e69b7744c1f5b3c09776
stef <stefan.marsiske@gmail.com>
2013-08-25 23:02:17 +0200

    [doc] better test.sh output

--- test.sh
+++ test.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/ksh
 
 rm -rf ./test-pbp
-# create some keys
+echo create some keys
+echo create alice
 ./pbp.py -g -n alice -b ./test-pbp || exit
+echo create bob
 ./pbp.py -g -n bob -b ./test-pbp || exit
+echo create carol
 ./pbp.py -g -n carol -b ./test-pbp || exit
 
 # test msg
@@ -11,31 +14,31 @@ cat >./test-pbp/howdy.txt <<EOF
 hello world
 EOF
 
-# public key crypto test
+echo public key crypto test
 ./pbp.py -c -S alice -r bob -i ./test-pbp/howdy.txt -b ./test-pbp || exit
-# decrypt
+echo decrypt
 ./pbp.py -d -S bob -i ./test-pbp/howdy.txt.pbp -b ./test-pbp || exit
 
-# secret key crypto test
+echo secret key crypto test
 ./pbp.py -c -i ./test-pbp/howdy.txt || exit
-# decrypt
+echo decrypt
 ./pbp.py -d -i ./test-pbp/howdy.txt.pbp || exit
 
-# public key signature test
+echo public key signature test
 ./pbp.py -s -S alice -i ./test-pbp/howdy.txt -b ./test-pbp || exit
-# verify
+echo verify
 ./pbp.py -v -i ./test-pbp/howdy.txt.sig -b ./test-pbp || exit
 
-# some key signing tests
+echo some key signing tests
 ./pbp.py -m -S alice -n bob -b ./test-pbp || exit
 ./pbp.py -m -S alice -n carol -b ./test-pbp || exit
 ./pbp.py -m -S bob -n carol -b ./test-pbp || exit
 
-# check sigs on carols key
+echo check sigs on carols key
 ./pbp.py -C -n carol -b ./test-pbp || exit
 ./pbp.py -C -n bob -b ./test-pbp || exit
 
-# test PFS mode
+echo test PFS mode
 rm ./test-pbp/sk/.alice/bob ./test-pbp/sk/.bob/alice /tmp/[24]bob* /tmp/[24]alice*
 ./pbp.py -e -S alice -r bob -b ./test-pbp -i ./test-pbp/howdy.txt -o /tmp/2bob || exit
 ./pbp.py -E -S bob -r alice -b ./test-pbp/ -i /tmp/2bob || exit
@@ -45,7 +48,7 @@ rm ./test-pbp/sk/.alice/bob ./test-pbp/sk/.bob/alice /tmp/[24]bob* /tmp/[24]alic
 ./pbp.py -E -S bob -r alice -b ./test-pbp/ -i /tmp/2bob || exit
 ./pbp.py -e -S bob -r alice -b ./test-pbp/ -i ./test-pbp/howdy.txt -o /tmp/2alice || exit
 ./pbp.py -E -S alice -r bob -b ./test-pbp/ -i /tmp/2alice -o /tmp/4alice || exit
-# test some repeated msgs
+echo test some repeated msgs
 ./pbp.py -e -S alice -r bob -b ./test-pbp -i ./test-pbp/howdy.txt -o /tmp/2bob || exit
 ./pbp.py -e -S alice -r bob -b ./test-pbp -i ./test-pbp/howdy.txt -o /tmp/2bob-2 || exit
 ./pbp.py -E -S bob -r alice -b ./test-pbp/ -i /tmp/2bob || exit
