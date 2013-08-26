ec3deb9d9a902313893459e63aa10c95153729dd
stef <stefan.marsiske@gmail.com>
2013-08-26 21:56:41 +0200

    [mod] revert prev commit

--- dhdemo-nacl.py
+++ dhdemo-nacl.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 
 import pysodium as nacl
 from base85 import b85encode
--- publickey.py
+++ publickey.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 import pysodium as nacl, scrypt # external dependencies
 import os, stat,  getpass, datetime, sys, struct, binascii
 from itertools import imap
