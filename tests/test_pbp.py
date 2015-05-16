46de6a53bd71fccdf04c947659587ba8a63b135f
stf <s@ctrlc.hu>
2015-05-16 02:03:50 +0200

    [mod] more tests

--- tests/test_pbp.py
+++ tests/test_pbp.py
@@ -98,6 +98,131 @@ class TestPBP(unittest.TestCase):
         self.assertTrue(publickey.verify(self_key.sign(MESSAGE, master=True),
             basedir=self.pbp_path, master=True) is not None)
 
+    def test_simple_dh(self):
+        (exp,pub1) = pbp.dh1_handler()
+        (pub2,secret) = pbp.dh2_handler(pub1)
+        self.assertEquals(pbp.dh3_handler(pub2,exp), secret)
+
+    def test_mpecdh(self):
+        publickey.Identity('alice', basedir=self.pbp_path, create=True)
+        publickey.Identity('bob', basedir=self.pbp_path, create=True)
+        publickey.Identity('carol', basedir=self.pbp_path, create=True)
+        pbp.mpecdh_start_handler('1st', 3, 'alice', '/dev/null', self.tmp_dir+ '/step1', basedir=self.pbp_path)
+        pbp.mpecdh_start_handler('1st', 3, 'bob', self.tmp_dir+'/step1', self.tmp_dir+'/step2', basedir=self.pbp_path)
+        s1=pbp.mpecdh_start_handler('1st', 3, 'carol', self.tmp_dir+'/step2', self.tmp_dir+'/step3', basedir=self.pbp_path)
+        s2=pbp.mpecdh_end_handler('1st', 'alice', self.tmp_dir+'/step3', self.tmp_dir+'/step4', basedir=self.pbp_path)
+        s3=pbp.mpecdh_end_handler('1st', 'bob', self.tmp_dir+'/step4', self.tmp_dir+'/step5', basedir=self.pbp_path)
+        self.assertEquals(s1,s2)
+        self.assertEquals(s2,s3)
+
+    def test_chaining(self):
+        publickey.Identity('alice', basedir=self.pbp_path, create=True)
+        publickey.Identity('bob', basedir=self.pbp_path, create=True)
+        msg=self.tmp_dir+'/msg'
+        msg2=self.tmp_dir+'/msg2'
+        msg3=self.tmp_dir+'/msg3'
+        msg4=self.tmp_dir+'/msg4'
+        ct=self.tmp_dir+'/ct'
+        ct2=self.tmp_dir+'/ct2'
+        ct3=self.tmp_dir+'/ct3'
+        with open(msg, 'w') as fd:
+            fd.write('0' * 1080)
+
+        pbp.chaining_encrypt_handler(infile=msg, outfile=ct, recipient='bob', self='alice', basedir=self.pbp_path)
+        pbp.chaining_decrypt_handler(infile=ct, outfile=msg2, recipient='bob', self='alice', basedir=self.pbp_path)
+
+        with open(msg2, 'r') as fd:
+            res = fd.read()
+        self.assertEquals(res, '0'*1080)
+
+        with open(msg, 'w') as fd:
+            fd.write('1' * 1080)
+
+        pbp.chaining_encrypt_handler(infile=msg, outfile=ct2, recipient='alice', self='bob', basedir=self.pbp_path)
+        pbp.chaining_decrypt_handler(infile=ct2, outfile=msg3, recipient='alice', self='bob', basedir=self.pbp_path)
+
+        with open(msg3, 'r') as fd:
+            res = fd.read()
+        self.assertEquals(res, '1'*1080)
+
+        #with open(msg, 'w') as fd:
+        #    fd.write('2' * 1080)
+
+        #pbp.chaining_encrypt_handler(infile=msg, outfile=ct3, recipient='bob', self='alice', basedir=self.pbp_path)
+        #pbp.chaining_decrypt_handler(infile=ct3, outfile=msg4, recipient='bob', self='alice', basedir=self.pbp_path)
+
+        #with open(msg4, 'r') as fd:
+        #    res = fd.read()
+        #self.assertEquals(res, '2'*1080)
+
+    def test_crypt(self):
+        publickey.Identity('alice', basedir=self.pbp_path, create=True)
+        publickey.Identity('bob', basedir=self.pbp_path, create=True)
+        publickey.Identity('carol', basedir=self.pbp_path, create=True)
+        msg=self.tmp_dir+'/msg'
+        msg2=self.tmp_dir+'/msg2'
+        ct=self.tmp_dir+'/ct'
+        with open(msg, 'w') as fd:
+            fd.write('0' * 1080)
+
+        pbp.encrypt_handler(infile=msg, outfile=ct, recipient=['bob','carol'], self='alice', basedir=self.pbp_path)
+        pbp.decrypt_handler(infile=ct, outfile=msg2, self='bob', peer='alice', max_recipients = 20, basedir=self.pbp_path)
+
+        with open(msg2, 'r') as fd:
+            res = fd.read()
+        self.assertEquals(res, '0'*1080)
+
+        pbp.decrypt_handler(infile=ct, outfile=msg2, self='carol', peer='alice', max_recipients = 20, basedir=self.pbp_path)
+
+        with open(msg2, 'r') as fd:
+            res = fd.read()
+        self.assertEquals(res, '0'*1080)
+
+    def test_sign_handler(self):
+        publickey.Identity('alice', basedir=self.pbp_path, create=True)
+        publickey.Identity('bob', basedir=self.pbp_path, create=True)
+        msg=self.tmp_dir+'/msg'
+        msg2=self.tmp_dir+'/msg2'
+        signed=self.tmp_dir+'/signed'
+        with open(msg, 'w') as fd:
+            fd.write('0' * 1080)
+
+        pbp.sign_handler(infile=msg, outfile=signed, self='alice', basedir=self.pbp_path)
+        sender = pbp.verify_handler(infile=signed, outfile=msg2, basedir=self.pbp_path)
+
+        with open(msg2, 'r') as fd:
+            res = fd.read()
+        self.assertEquals(res, '0'*1080)
+        self.assertEquals(sender, 'alice')
+
+    def test_keysign(self):
+        publickey.Identity('alice', basedir=self.pbp_path, create=True)
+        publickey.Identity('bob', basedir=self.pbp_path, create=True)
+        pbp.keysign_handler(name='bob', self='alice', basedir=self.pbp_path)
+        sigs=pbp.keycheck_handler(name='bob', basedir=self.pbp_path)
+        self.assertEquals(sigs, ['alice'])
+
+    def test_export(self): # also tests import
+        publickey.Identity('alice', basedir=self.pbp_path, create=True)
+        publickey.Identity('bob', basedir=self.tmp_dir, create=True)
+        export = pbp.export_handler('alice', basedir=self.pbp_path)
+        key=self.tmp_dir+'/key.exp'
+        with open(key, 'w') as fd:
+            fd.write(export)
+        pbp.import_handler(infile=key, basedir=self.tmp_dir)
+        pks=publickey.get_public_keys(basedir=self.tmp_dir)
+        self.assertEquals([p.name for p in pks if p.name!='bob'], ['alice'])
+
+    def test_hash(self):
+        msg=self.tmp_dir+'/msg'
+        with open(msg, 'w') as fd:
+            fd.write('0' * 1080)
+        h=pbp.hash_handler(infile=msg, k='', outlen=16)
+        self.assertEquals(h, '\xb9y\x06\x90\xb1O\x93\x89\x00K\xb2.\xcbE$G')
+        h=pbp.hash_handler(infile=msg, k='some random "key" with 32 byte output', outlen=32)
+        print repr(h)
+        self.assertEquals(h,'\xb9y\x06\x90\xb1O\x93\x89\x00K\xb2.\xcbE$G\xbf1\xfd\t\x96\x1c\xae\x95\x03\x99\xad\xf9\xb5\xff\xb1\x8d')
+
     def tearDown(self):
         rmtree(self.tmp_dir)
 
