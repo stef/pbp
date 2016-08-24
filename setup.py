b328f7af5f20f512ce8c8c4e1fa6aa769faa3a59
stf <s@ctrlc.hu>
2016-08-24 20:45:04 +0200

    [mod] bumped version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name = "pbp",
-    version = "0.3.1",
+    version = "0.3.2",
     author = "Stefan Marsiske",
     author_email = "s@ctrlc.hu",
     description = ("simple crypto tool"),
